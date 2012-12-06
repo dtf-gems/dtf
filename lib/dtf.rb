d35fc62d213572e7a53c56411f155395921e97ce
ddd <me@daviddwdowney.com>
2012-12-06 11:10:04 -0500

    Cleaned up whitespace, and reformatted lib/dtf.rb
    * Formatting of the file was incorrect. Fixed.

--- lib/dtf.rb
+++ lib/dtf.rb
@@ -41,7 +41,7 @@ module Dtf
       global_opts = Trollop::options do
         version "DTF v#{Dtf::VERSION}"
         banner <<-EOS
-#{version}
+        #{version}
         (c) Copyright 2012 David Deryl Downey / Deryl R. Doucette. All Rights Reserved.
         This is free software; see the LICENSE file for copying conditions.
         There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -56,33 +56,33 @@ module Dtf
         stop_on SUB_COMMANDS
       end
 
-      cmd = arg.shift
+      cmd      = arg.shift
       cmd_opts = case cmd
                    when "create_user"
                      Trollop::options do
-                       opt(:user_name, desc="Username for new TF user - REQUIRED", opts={:type => :string, :short => '-u'})
-                       opt(:full_name, desc="Real name for new TF user - REQUIRED", opts={:type => :string, :short => '-n'})
-                       opt(:email_address, desc="Email address for new TF user - REQUIRED", opts={:type => :string, :short => '-e'})
+                       opt(:user_name, desc="Username for new TF user - REQUIRED", opts={ :type => :string, :short => '-u' })
+                       opt(:full_name, desc="Real name for new TF user - REQUIRED", opts={ :type => :string, :short => '-n' })
+                       opt(:email_address, desc="Email address for new TF user - REQUIRED", opts={ :type => :string, :short => '-e' })
                      end
                    when "create_vs"
                      Trollop::options do
-                       opt(:user_name, desc="TF user to associate this VS with - REQUIRED", opts={:type => :string, :short => '-u'})
-                       opt(:name, desc="Name for new VS - REQUIRED", opts={:type => :string, :short => '-n'})
-                       opt(:description, desc="Description of VS's intended use - OPTIONAL", opts={:type => :string, :short => '-d', :default => ''})
+                       opt(:user_name, desc="TF user to associate this VS with - REQUIRED", opts={ :type => :string, :short => '-u' })
+                       opt(:name, desc="Name for new VS - REQUIRED", opts={ :type => :string, :short => '-n' })
+                       opt(:description, desc="Description of VS's intended use - OPTIONAL", opts={ :type => :string, :short => '-d', :default => '' })
                      end
                    when "delete_user"
                      Trollop::options do
-                       opt(:user_name, desc="Username of TF user to delete - REQUIRED", opts={:type => :string, :short => '-u'})
+                       opt(:user_name, desc="Username of TF user to delete - REQUIRED", opts={ :type => :string, :short => '-u' })
                        opt(:delete_all, desc="Delete _all_ VSs this user owns", :type => :flag, :default => true)
                      end
                    when "delete_vs"
                      Trollop::options do
-                       opt(:user_name, desc="Username of VS owner - REQUIRED", opts={:type => :string, :short => '-u'})
-                       opt(:id, desc="ID of VS to be deleted - REQUIRED", opts={:type => :int, :short => '-i'})
+                       opt(:user_name, desc="Username of VS owner - REQUIRED", opts={ :type => :string, :short => '-u' })
+                       opt(:id, desc="ID of VS to be deleted - REQUIRED", opts={ :type => :int, :short => '-i' })
                      end
                    when "setup_dtf"
                      Trollop::options do
-                       opt(:install, desc="Defines if should install or not", opts={:type => :flag, :default => true})
+                       opt(:install, desc="Defines if should install or not", opts={ :type => :flag, :default => true })
                      end
                    when nil
                      Trollop::die "No command specified! Please specify an applicable command"
